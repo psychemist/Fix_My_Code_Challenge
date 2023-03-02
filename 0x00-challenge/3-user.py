44a3271258446c67991a24aa5571e3ae28093206
psych0wh1z <ikeagudike@gmail.com>
2023-03-02 18:18:46 +0100

    Fix task4 file

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- User Model
+User Model
 """
 import hashlib
 import uuid
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
@@ -78,11 +78,11 @@ if __name__ == '__main__':
 
     user_2.password = None
     if user_2.password is not None:
-        print("User.password should be None if setter to None")
+        print("User.password should be None if set to None")
 
     user_2.password = 89
     if user_2.password is not None:
-        print("User.password should be None if setter to an integer")
+        print("User.password should be None if set to an integer")
 
     if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
@@ -93,10 +93,10 @@ password")
 password")
 
     if user_1.is_valid_password(None):
-        print("is_valid_password should return False if compare with None")
+        print("is_valid_password should return False if compared with None")
 
     if user_1.is_valid_password(89):
-        print("is_valid_password should return False if compare with integer")
+        print("is_valid_password should return False if compared with integer")
 
     if user_2.is_valid_password("No pwd"):
         print("is_valid_password should return False if no password set \
