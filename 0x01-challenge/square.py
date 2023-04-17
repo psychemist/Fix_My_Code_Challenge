718f930ac57398f0a886b31fa58365cee2dcb102
psych0wh1z <ikeagudike@gmail.com>
2023-04-17 22:53:04 +0100

    Add docstrings

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -13,10 +13,11 @@ class Square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Initializes square class instance and populates its dictionary """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
+    def area(self):
         """ Returns area of the square """
         return self.width * self.height
 
@@ -30,7 +31,8 @@ class Square():
 
 
 if __name__ == "__main__":
+
     s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
+    print(s.area())
     print(s.perimeter_of_my_square())
